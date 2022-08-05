13c5e225e2fe3aafc38dc6a6d37f9fc0b0134822
Aphaki <kiapha1108@gmail.com>
2022-08-05 16:27:10 +0900

    Initial Commit

---
+++ HandGosi/HandGosiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandGosiApp.swift
+//  HandGosi
+//
+//  Created by Sy Lee on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct HandGosiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
