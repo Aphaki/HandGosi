2a32c4830dfb2f4f06f4dc0946f619fb4190963e
Aphaki <kiapha1108@gmail.com>
2022-12-05 20:36:02 +0900

    App 파일쪽 변경

--- HandGosi/HandGosiApp.swift
+++ HandGosi/HandGosiApp.swift
@@ -13,14 +13,27 @@ import GoogleMobileAds
 struct HandGosiApp: App {
     
     init() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+            if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+                //User has not indicated their choice for app tracking
+                //You may want to show a pop-up explaining why you are collecting their data
+                //Toggle any variables to do this here
+            } else {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    //Whether or not user has opted in initialize GADMobileAds here it will handle the rest
+                                                                
+                    GADMobileAds.sharedInstance().start(completionHandler: nil)
+                }
+            }
+        }
+//    init() {
+//        GADMobileAds.sharedInstance().start(completionHandler: nil)
         
 //        DispatchQueue.global(qos: .default).asyncAfter(deadline: .now() + 1) {
 //            ATTrackingManager.requestTrackingAuthorization { _ in
 //
 //            }
 //        }
-    }
+//    }
     
     @StateObject var mainVM = MainVM()
     
